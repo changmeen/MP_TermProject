89edb62b96fda4567bad7eb284e265a4375ab725
Jaeuk Kim <ksyj2006@naver.com>
2021-05-17 01:01:30 +0900

    해낸것 같기도 ..

--- app/src/main/java/com/changmeen/firebase/TodayMenu.java
+++ app/src/main/java/com/changmeen/firebase/TodayMenu.java
@@ -1,47 +1,41 @@
 package com.changmeen.firebase;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.LinearLayout;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
+import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
+
 public class TodayMenu extends Fragment {
+    AutoScrollViewPager autoScrollViewPager;
+    ArrayList<String> data = new ArrayList<>();
 
-    ViewGroup viewGroup;
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.today_menu,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.today_menu, container, false);
+        LinearLayout layout = (LinearLayout) v.findViewById(R.id.todaymenu);
 
-        Button subButton1 = (Button) viewGroup.findViewById(R.id.button1);
-        Button subButton2 = (Button) viewGroup.findViewById(R.id.button2);
-        Button subButton3 = (Button) viewGroup.findViewById(R.id.button3);
+        ArrayList<Object> data = new ArrayList<>(); //이미지 url를 저장하는 arraylist
+        data.add(R.drawable.kimchibap);
+        data.add(R.drawable.bibimbap);
+        data.add(R.drawable.tteokbok2);
+        data.add(R.drawable.soysauceeggbap);
 
-        subButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getFragmentManager().beginTransaction().replace(R.id.Layout1, new child1()).addToBackStack(null).commit();
-            }
-        });
-        subButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getFragmentManager().beginTransaction().replace(R.id.Layout1, new child2()).addToBackStack(null).commit();
-            }
-        });
-        subButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               Intent intent = new Intent(getActivity(), youtube.class);
-               startActivity(intent);
-            }
-        });
-        return viewGroup;
+
+        autoScrollViewPager = (AutoScrollViewPager) v.findViewById(R.id.autoViewPager);
+
+        AutoScrollAdapter scrollAdapter = new AutoScrollAdapter(getActivity(), data);
+        autoScrollViewPager.setAdapter(scrollAdapter); //Auto Viewpager에 Adapter 장착
+        autoScrollViewPager.setInterval(3000); // 페이지 넘어갈 시간 간격 설정
+        autoScrollViewPager.startAutoScroll(); //Auto Scroll 시작
+
+        return v;
+
     }
 }
