abd5a8f3e23a6374dc3ed5c0d8181102a33a13e3
Jaeuk Kim <ksyj2006@naver.com>
2021-05-17 23:01:14 +0900

    데이터베이스 가져오기 아직 미완료 ..

--- app/src/main/java/com/changmeen/firebase/TodayMenu.java
+++ app/src/main/java/com/changmeen/firebase/TodayMenu.java
@@ -1,18 +1,29 @@
 package com.changmeen.firebase;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
 
 public class TodayMenu extends Fragment {
+
+    private DatabaseReference mDatabase;
     AutoScrollViewPager autoScrollViewPager;
     ArrayList<String> data = new ArrayList<>();
 
@@ -21,11 +32,35 @@ public class TodayMenu extends Fragment {
         View v = inflater.inflate(R.layout.today_menu, container, false);
         LinearLayout layout = (LinearLayout) v.findViewById(R.id.todaymenu);
 
-        ArrayList<Object> data = new ArrayList<>(); //이미지 url를 저장하는 arraylist
-        data.add(R.drawable.kimchibap);
-        data.add(R.drawable.bibimbap);
-        data.add(R.drawable.tteokbok2);
-        data.add(R.drawable.soysauceeggbap);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        EditText et = (EditText) v.findViewById(R.id.et);
+
+        mDatabase.child("Food").child("Noodle").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    String readstr;
+                    readstr = String.valueOf(task.getResult().getValue());
+
+                    while(readstr.indexOf("image") != -1){
+                        int sidx = readstr.indexOf("image") + 6;
+                        int eidx = readstr.indexOf(",");
+
+                        et.setText(readstr.substring(sidx,eidx));
+
+                        readstr = readstr.substring(eidx);
+
+                        if(readstr.indexOf("image") != -1)
+                            readstr = readstr.substring(readstr.indexOf("image"));
+                    }
+
+                }
+            }
+        });
 
 
         autoScrollViewPager = (AutoScrollViewPager) v.findViewById(R.id.autoViewPager);
