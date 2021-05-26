391d8ae0d40eae8ac51acc551c94b3a75073711d
kim-jeonggyu <sosqre0105@naver.com>
2021-05-26 22:38:14 +0900

    3번째 fragment 바꿨슴다

---
+++ app/src/main/java/com/changmeen/firebase/Rfg_page.java
@@ -0,0 +1,41 @@
+package com.changmeen.firebase;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Rfg_page extends Fragment {
+    //왜 안들어가짐
+
+    private RecyclerView listview1;
+    private RecyclerView listview2;
+    Ingredient_adapter adapter1;
+    private rec_adapter adapter2;
+    private Context mContext;
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.rfg_page, container, false);
+
+        ImageButton next = view.findViewById(R.id.rfg_Button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Ingredient_page.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        return view;
+    }
+}
